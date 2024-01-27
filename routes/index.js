0a6b979d41b8b4c8a280db25e27f89ecedaaa451
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-01-27 11:44:16 +0530

    comments and css portions

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', homeController.home);
 
 // Use the correct path for the "users" route
 router.use('/users', require('./users'));
+router.use('/posts',require('./posts'));
 //for any further routes, access from here
 //router.use('/routerName',require('./routerfile));
 
