a4046614d121ef57cc6e1925d2e0543785c5e45e
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-02-08 20:32:10 +0530

    Setting Up Passport JWT

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,12 @@
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+const User = require('../models/user');
+
+let opts = {
+    jwtFromRequest : ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey : 'codeial'
+}
+
+passport
